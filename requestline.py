cdaab8b46e40c0b920b0f73d983716559d851bcf
swickape <patrick.swickard@amplyfigroup.com>
2023-06-13 19:42:41 -0400

    postgram corrections

--- requestline.py
+++ requestline.py
@@ -1,7 +1,7 @@
 import requests
 import re
 
-poem_number = '30'
+poem_number = '14'
 poemfilename = 'spsidebyside' + str(poem_number) + '.txt'
 with open(poemfilename, 'r') as file:
     thisfile = file.read().split('\n')
@@ -108,7 +108,7 @@ def request_anagram_from_site(section):
   else:
     return []
 
-linecount = 14
+linecount = 30
 for thisline in bigarray[linecount:]:
   linecount += 1
   sectioncount = 0
